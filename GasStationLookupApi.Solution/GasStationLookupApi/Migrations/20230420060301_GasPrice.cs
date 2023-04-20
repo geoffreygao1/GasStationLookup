2385eaee6f720f7ba1e2eb8dc44322804ad84f0c
Geoffrey Gao <geoffreygao1@gmail.com>
2023-04-20 15:03:17 +0900

    Add Migrations

---
+++ GasStationLookupApi.Solution/GasStationLookupApi/Migrations/20230420060301_GasPrice.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GasStationLookupApi.Migrations
+{
+    public partial class GasPrice : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "GasPrice",
+                columns: table => new
+                {
+                    GasPriceId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Date = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    Diesel = table.Column<float>(type: "float", nullable: false),
+                    Unleaded = table.Column<float>(type: "float", nullable: false),
+                    Premium = table.Column<float>(type: "float", nullable: false),
+                    StationId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GasPrice", x => x.GasPriceId);
+                    table.ForeignKey(
+                        name: "FK_GasPrice_Station_StationId",
+                        column: x => x.StationId,
+                        principalTable: "Station",
+                        principalColumn: "StationId",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_GasPrice_StationId",
+                table: "GasPrice",
+                column: "StationId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "GasPrice");
+        }
+    }
+}
