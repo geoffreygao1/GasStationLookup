c7a3175cd38882ddbd7980c62bfad8f03286d896
Geoffrey Gao <geoffreygao1@gmail.com>
2023-04-20 14:46:43 +0900

    Added packages

--- GasStationLookupApi.Solution/GasStationLookupApi/Program.cs
+++ GasStationLookupApi.Solution/GasStationLookupApi/Program.cs
@@ -12,8 +12,12 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+  app.UseSwagger();
+  app.UseSwaggerUI();
+}
+else
+{
+  app.UseHttpsRedirection();
 }
 
 app.UseHttpsRedirection();
